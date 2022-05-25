4c4ff5d8be56cfb02dd795395e8891830e577466
Diego Echalar Penailillo <diegoechalarp@hotmail.com>
2022-05-25 11:59:05 -0400

    Actualización 11:58

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from 'react';
 import './App.css';
+import freeCodeCampLogo from './imagenes/freeCodeCampLogo.png';
+import Tarea from './Componentes/tarea';
 
 function App() {
   return (
-    <div className="App">
-      React
+    <div className='aplicacion-tareas'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img src={freeCodeCampLogo} className='freecodecamp-logo' alt="Logo" />
+      </div>
+      <div className='tareas-lista-principal'>
+        <h1>Mis Tareas</h1>
+        <Tarea texto='Aprender React' />
+      </div>
     </div>
   );
 }
